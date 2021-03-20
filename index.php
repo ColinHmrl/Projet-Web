1c82d32c26f285d93f064ab8fbc918b6644f369f
Adrien-S <73822547+Adrien-S@users.noreply.github.com>
2021-03-20 19:53:51 +0100

    Clean up + Begin of the project

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
 <?php
 
-
-require_once 'assets/vendors/autoload.php';
-
-$loader = new \Twig\Loader\FilesystemLoader(__DIR__.'/vue');
-$twig = new \Twig\Environment($loader, [
-    'cache' => false, //__DIR__.'/cache'
-]);
-
-//$template = $twig->load('index.html');
-
-
-
-echo $twig->render('index2.html', ['name' => 'Fabien']);
+include("controlers/controler.php");
