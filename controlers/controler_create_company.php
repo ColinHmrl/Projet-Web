94ddd0388d2a73ae40ea60d4e5b2c2f7a7c223c4
Colin-hmrl <Hamerel.co@gmail.com>
2021-03-23 19:32:32 +0100

    Update create company / non finie

--- controlers/controler_create_company.php
+++ controlers/controler_create_company.php
@@ -1,10 +1,22 @@
 <?php
-require_once 'assets/vendors/autoload.php';
-require 'models/model.php';
+session_start();
+require_once '../assets/vendors/autoload.php';
+require '../models/model_create_company.php';
 
 $loader = new \Twig\Loader\FilesystemLoader(__DIR__.'/../vues');
 $twig = new \Twig\Environment($loader, [
     'cache' => false, //__DIR__.'/cache'
 ]);
-echo twig->render('create_company.html');
+
+if(isset($_POST['name'])){
+    post_form($_POST['name'],$_POST['description'],$_POST['locality'],$_POST['activity_area'],$_POST['email']);
+    echo 'succeed';
+    return;
+    }
+    else{
+        echo $twig->render('create_company.html');
+    }
+
+
+
 ?>
