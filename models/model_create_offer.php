1103052ab489f4b30ad68885b7fb009a96e301b7
Skywig08 <73820854+Skywig08@users.noreply.github.com>
2021-03-30 11:28:08 +0200

    m

--- models/model_create_offer.php
+++ models/model_create_offer.php
@@ -1,27 +1,61 @@
 <?php
 
-function post_form($entreprise,$location,$compétence,$offertitle,$placeavaible,$promoselect,$rémunération,$description){
+function post_form($offertitle,$location,$offer_date,$training_period,$rémunération,$entreprise,$placeavaible,$promoselect,$description,){
+    $date_post = date('y-m-d h:i:s');
     include('loginBDD.php');
     
     if($_POST){ 
         // var_dump($_POST);
         try {
             //$dbh = new PDO('mysql:host=localhost;dbname=projetweb', 'root', 'root');
-            $sql = "INSERT INTO offer ( locality, promotion_concerned, training_period, remuneration_basis, offer_date, nb_places, del, id_company, description) VALUES (:locality, :promotion_concerned, :training_period, :remuneration_basis, :offer_date, :id_company, :description)"; 
+            $sql = "INSERT INTO offer (locality_offer, training_period, remuneration_basis, offer_date, nb_places, date_post, title, description, del, id_company) VALUES ( :locality, :training_period, :remuneration_basis, :offer_date, :nb_places, :date_post, :title, :description, :del, :id_company, )"; 
             $prepared = $bdd->prepare($sql);
             $prepared->execute([
-               ':promotion_concerned' => $promoselect,
-               ':nb_places' => $placeavaible,
-               ':invisible' => 0,
-               ':del' => 0,
-               ':description' => $description,
-               ':remuneration_basis' => $rémunération,
+                ':locality'=> $location,
+                ':training_period'=> $training_period,
+                ':remuneration_basis'=> $rémunération,
+                ':offer_date'=> $offer_date,
+                ':title'=> $offertitle,
+                ':date_post'=> $date_post,
+                ':nb_places' => $placeavaible,
+                ':invisible' => 0,
+                ':del' => 0,
+                ':description' => $description,
+                
+
             ]);
         }
         catch (PDOException $e) {
             print "Error!: " . $e->getMessage() . "<br/>";
             die();
         }
+    }
+}
+
+
+function get_company($id){
+    try{
+        include('loginBDD.php');
+        $sql = "SELECT id, name, activity_area, locality, email, invisible, del, description FROM company";
+        $prepared = $bdd->prepare($sql);
+
+        if(!$prepared->execute([
+            ':id' => $id
+        ])){
+            print_r($bdd->errorInfo());
+        }
+
+        $result = $prepared->fetch();
+        return $result;
+
+    }
+    catch(\Exception $e)
+    {
+            die('Erreur : '.$e->getMessage());
+    }
+    catch(\PDOException $e)
+    {
+            die('Erreur : '.$e->getMessage());
     }
 }
 ?>
